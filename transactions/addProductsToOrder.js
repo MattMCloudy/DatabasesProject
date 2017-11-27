e3fc705a1f1d919cbe0ca39151dea370cca591b2
Matthew McLeod <matthew.mcleod@mavs.uta.edu>
2017-11-26 18:05:48 -0600

    adding payments and prices to orders

--- transactions/addProductsToOrder.js
+++ transactions/addProductsToOrder.js
@@ -1,4 +1,41 @@
+function setOrderPriceMaker(info, client) {
+    return (results) => {
+        client.update({
+            table: 'Orders',
+            updates: [
+                {
+                    key: 'total_price',
+                    value: results[0]['SUM(Price)']
+                }
+            ],
+            conditions: [
+                {
+                    key: 'Order_ID',
+                    value: info['OrderId']
+                }
+            ]
+        })
+    }
+}
+
+function updateOrderPriceMaker (info, client) {
+    var setOrderPrice = setOrderPriceMaker(info, client);
+    return (results) => {
+        client.select({
+            tables: ['Products'],
+            fields: ['SUM(Price)'],
+            conditions: [
+                {
+                    key: 'OrderId',
+                    value: info['OrderId']
+                }
+            ]
+        }, setOrderPrice);
+    }
+}
+
 function addProductsToOrder(info, client) {
+    var updateOrderPrice = updateOrderPriceMaker(info, client);
     var product_ids = info['ProductIds'].split(',')
     product_ids.forEach(function(element) {
         client.update({
@@ -17,6 +54,7 @@ function addProductsToOrder(info, client) {
            ] 
         });
     });
+    setTimeout(updateOrderPrice, 5000);
 }
 
 module.exports = addProductsToOrder;
--- transactions/makePayment.js
+++ transactions/makePayment.js
@@ -1,8 +1,39 @@
+function updateTotalPaidCallbackMaker(payment, client) {
+    return (results) => {
+        client.update({
+            table: 'Orders',
+            updates: [
+                {
+                    key: 'total_amt_paid',
+                    value: results[0]['SUM(Amt_paid)']
+                }
+            ],
+            conditions: [
+                {
+                    key: 'Order_ID',
+                    value: payment['OrderID']
+                }
+            ]
+        });
+    }
+}
+
 function insertCallbackMaker(payment, client) {
+    var updateTotalPaidCallback = updateTotalPaidCallbackMaker(payment, client);
     return (results) => {
         var current_count = results[0]['COUNT(*)'];
         payment['Payment_ID'] = current_count + 1;
         client.insert.customer_payment(payment);
+        client.select({
+            tables: ['Customer_Payments'],
+            fields: ['SUM(Amt_paid)'],
+            conditions: [
+                {
+                    key: 'OrderID',
+                    value: payment['OrderID']
+                }
+            ]
+        }, updateTotalPaidCallback)
     }
 }
 
